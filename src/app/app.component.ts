8ceb7275228ae96f79adb5f0deea421c92e02583
Anton Lashan <antonlashan@gmail.com>
2020-11-07 21:52:59 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,69 @@
 import { Component } from '@angular/core';
 
+interface IItem {
+  id: number;
+  name: string;
+}
+
+type ISelectedItem = Pick<IItem, 'id'>;
+
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'simple-listbox';
+  list1: IItem[] = [
+    { id: 1, name: 'item 1' },
+    { id: 2, name: 'item 2' },
+    { id: 3, name: 'item 3' },
+    { id: 4, name: 'item 4' },
+  ];
+  list2: IItem[] = [
+    { id: 5, name: 'item 5' },
+    { id: 6, name: 'item 6' },
+    { id: 7, name: 'item 7' },
+  ];
+
+  selectedList1: ISelectedItem[] = [{ id: 1 }, { id: 3 }];
+  selectedList2: ISelectedItem[] = [{ id: 6 }, { id: 7 }];
+
+  moveSelected(direction) {
+    if (direction === 'left') {
+      this.selectedList2.forEach((item) => {
+        // find mapped element
+        const itemInList2 = this.list2.find((l2) => l2.id === item.id);
+
+        this.list1.push(itemInList2); // add to list 1
+        this.list2 = this.list2.filter((l2) => l2.id !== item.id); // remove from list 2
+      });
+      // clear selected list 2
+      this.selectedList2 = [];
+    } else {
+      this.selectedList1.forEach((item) => {
+        // find mapped element
+        const itemInList1 = this.list1.find((l1) => l1.id === item.id);
+        this.list2.push(itemInList1); // add to list 2
+        this.list1 = this.list1.filter((l1) => l1.id !== item.id); // remove from list 1
+      });
+      // clear selected list 1
+      this.selectedList1 = [];
+    }
+  }
+
+  moveAll(direction) {
+    if (direction === 'left') {
+      this.list1 = [...this.list1, ...this.list2];
+      this.list2 = [];
+      this.selectedList2 = [];
+    } else {
+      this.list2 = [...this.list2, ...this.list1];
+      this.list1 = [];
+      this.selectedList1 = [];
+    }
+  }
+
+  customCompare(o1: IItem, o2: IItem) {
+    return o1.id == o2.id;
+  }
 }
